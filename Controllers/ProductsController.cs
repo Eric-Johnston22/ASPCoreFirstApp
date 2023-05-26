90605bf783342213a9c453b80bb33b97f102ec86
Eric-Johnston <eric.mjohnston@protonmail.com>
2023-05-26 11:53:20 -0400

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace ASPCoreFirstApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        /**
+        public string Welcome(string name, int secretNumber = 13)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + " the secret number is " + secretNumber);
+        }
+        **/
+
+        public IActionResult Welcome()
+        {
+            ViewBag.name = "Eric";
+            ViewBag.secretNumber = 13;
+            return View();
+        }
+    }
+}
